19e04d8eb8afddf24bc9378fa8c8a86a55386863
Njord Lokebo <njordml@gmail.com>
2020-05-24 15:12:00 +0200

    -LED and Chargeur actionneur men bare stukturen, ikke innhold. Prøve fortsatt å finne ut kordan man linke de sammen eheh

--- src/LED.cpp
+++ src/LED.cpp
@@ -4,16 +4,20 @@
 LED::LED(int t):Actionneur(),temps(t){
 }
 
-void LED::run(){
-  /*while(1){
-    if(ptrmem!=NULL)
+LED::LED(int t):Actionneur(),state(LOW),temps(t){
+  
+  //while loop qui regarde l'etat de CapteurPresence (si la voiture n'est pas la - LED OFF)
+  //et CheckBatterie (si la batterie est en dessous de x - RED, en train de charger - ORANGE, à 100% - GREEN )
+  while(1){
+    /*if(ptrmem!=NULL)
       state=*ptrmem;
     if (state==LOW)
-      cout << "((((eteint))))\n";
+     ;
     else
-    cout << "((((allume))))\n";
+     ;
     sleep(temps);
-    }*/
+    */
+   }
 }
 
 void LED::GetColor(){
