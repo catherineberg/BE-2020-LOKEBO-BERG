0176e97a3776292924be825e46a37d0ed55bbe4d
hermunddossland <hermundld@gmail.com>
2020-05-25 15:42:24 +0200

    fix

--- src/board.cpp
+++ src/board.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "core_simulation.h"
 #include "mydevices.h"
 #include "capteur.h"
@@ -39,33 +38,3 @@ int main(){
   esp8266.run();
   return 0;
 }
-=======
-#include "core_simulation.h"
-#include "mydevices.h"
-<<<<<<< HEAD
-#include "actionneur.h"
-#include "LED.h"
-=======
-//CATHY TEEEEESTER ENGANG TIL
->>>>>>> my_new_branch
-
-int main(){
-  // creation d'une board
-  Board esp8266;
-  // achat des senseurs et actionneurs
-  AnalogSensorTemperature temperature(DELAY,TEMP);
-  DigitalActuatorLED led1(DELAY);
-  I2CActuatorScreen screen;
-  
-  // branchement des capteurs actionneurs
-  esp8266.pin(1,temperature);
-  esp8266.pin(0,led1);
-  esp8266.i2c(1,screen);
-  
-  // allumage de la carte
-  esp8266.run();
-  return 0;
-}
-
-
->>>>>>> ActLED+ActChargeur
