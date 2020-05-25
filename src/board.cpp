4a20466cff129a9982d5f9badd5a9616ff8c0f39
Njord Lokebo <njordml@gmail.com>
2020-05-25 14:09:25 +0200

    bygde videre på LEDen

--- src/board.cpp
+++ src/board.cpp
@@ -1,53 +1,7 @@
-<<<<<<< HEAD
 #include "core_simulation.h"
 #include "mydevices.h"
-#include "capteur.h"
-#include "luminosity.h"
-#include "button.h"
-#include "porte.h"
-
-
-int main(){
-  // creation d'une board
-  Board esp8266;
-  // achat des senseurs et actionneurs
-  AnalogSensorTemperature temperature(DELAY,TEMP);
-  DigitalActuatorLED led1(DELAY);
-  I2CActuatorScreen screen;
-
-  //%%%%%%%
-  AnalogSensorLuminosity luminosite(DELAY, luminosite_environnement);
-  IntelligentDigitalActuatorLED led2(DELAY);
-  Button button1(LOW, DELAY);
-
-  //%%%%%%
-  // initialisation du materiel
-  //%%%%%%%%%%%%%%%%%%%%%ù
-
-  //%%%%%%%%%%%%%%%%%%%
-
-  // branchement des capteurs actionneurs
-  esp8266.pin(1,temperature);
-  esp8266.pin(0,led1);
-  esp8266.i2c(1,screen);
-  //%%%%%%%%%%%%%%%%%
-  esp8266.pin(2,luminosite);
-  esp8266.pin(3,led2);
-  esp8266.pin(4,button1);
-  //%%%%%%%%%%%%%%%%%%%
-  // allumage de la carte
-  esp8266.run();
-  return 0;
-}
-=======
-#include "core_simulation.h"
-#include "mydevices.h"
-<<<<<<< HEAD
 #include "actionneur.h"
 #include "LED.h"
-=======
-//CATHY TEEEEESTER ENGANG TIL
->>>>>>> my_new_branch
 
 int main(){
   // creation d'une board
