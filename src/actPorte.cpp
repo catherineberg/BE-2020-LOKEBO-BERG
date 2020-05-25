dc87913c20f2e199bd728898d3fb69834e3ad97b
Catherine Berg <catherineberg@live.no>
2020-05-25 18:57:13 +0200

    Kobla actPorte til PIN

--- src/actPorte.cpp
+++ src/actPorte.cpp
@@ -18,3 +18,14 @@ void actPorte:: close(){
     p.setPorte(0); //døren er lukket
 }
 
+void actPorte::run(){
+  while(1){
+    if(ptrmem!=NULL)
+      state=*ptrmem;
+    if (state==LOW)
+        p.setPorte(0); //If state er low, så lukker vi døren
+    else
+        p.setPorte(1); //If state er high, så åpner vi døren
+    sleep(temps);
+    }
+}
