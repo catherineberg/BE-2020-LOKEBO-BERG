d2bffc4dd2539ab79a7bc51d5b828839a444509b
hermunddossland <hermundld@gmail.com>
2020-05-25 13:19:59 +0200

    7

--- compile_linux.sh
+++ compile_linux.sh
@@ -1,3 +1,3 @@
  #!/usr/bin/bash
-echo "g++ -Wall -std=c++11 -lpthread src/board.cpp src/core_simulation.cpp src/mydevices.cpp src/sketch_ino.cpp -o arduino"
-g++ -Wall -std=c++11 -pthread src/board.cpp src/core_simulation.cpp src/mydevices.cpp src/sketch_ino.cpp -o arduino
+echo "g++ -Wall -std=c++11 -lpthread src/board.cpp src/core_simulation.cpp src/mydevices.cpp src/sketch_ino.cpp src/button.cpp src/porte.cpp -o arduino"
+g++ -Wall -std=c++11 -pthread src/board.cpp src/core_simulation.cpp src/mydevices.cpp src/sketch_ino.cpp src/button.cpp src/porte.cpp -o arduino
