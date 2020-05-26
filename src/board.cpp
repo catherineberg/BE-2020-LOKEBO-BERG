1e7671a625d5eaab442ccb69fc37f48b26e472a9
Njord Lokebo <njordml@gmail.com>
2020-05-26 13:17:16 +0200

    made changes in board.cpp and sketch_ino to fix merge conflicts

--- src/board.cpp
+++ src/board.cpp
@@ -4,8 +4,9 @@
 #include "luminosity.h"
 #include "button.h"
 #include "porte.h"
-#include "led.cpp"
-
+#include "actPorte.hpp"
+#include "actPorte.h"
+#include "ledc.h"
 
 int main(){
   // creation d'une board
@@ -22,7 +23,6 @@ int main(){
   AnalogSensorLuminosity luminosite(DELAY, luminosite_environnement);
   IntelligentDigitalActuatorLED led2(DELAY);
   Button button1(LOW, DELAY);
-
 
   //%%%%%%
   // initialisation du materiel
@@ -49,3 +49,38 @@ int main(){
 }
 
 
+
+int main(){
+  // creation d'une board
+  Board esp8266;
+  // achat des senseurs et actionneurs
+  AnalogSensorTemperature temperature(DELAY,TEMP);
+  DigitalActuatorLED led1(DELAY);
+  I2CActuatorScreen screen;
+  actPorte actporte1;
+
+  //%%%%%%%
+  AnalogSensorLuminosity luminosite(DELAY, luminosite_environnement);
+  IntelligentDigitalActuatorLED led2(DELAY);
+  Button button1(LOW, DELAY);
+  esp8266.pin(5,actporte1); //Kobler til servomotoren til en pin 
+
+  //%%%%%%
+  // initialisation du materiel
+  //%%%%%%%%%%%%%%%%%%%%%ù
+
+  //%%%%%%%%%%%%%%%%%%%
+
+  // branchement des capteurs actionneurs
+  esp8266.pin(1,temperature);
+  esp8266.pin(0,led1);
+  esp8266.i2c(1,screen);
+  //%%%%%%%%%%%%%%%%%
+  esp8266.pin(2,luminosite);
+  esp8266.pin(3,led2);
+  esp8266.pin(4,button1);
+  //%%%%%%%%%%%%%%%%%%%
+  // allumage de la carte
+  esp8266.run();
+  return 0;
+}
