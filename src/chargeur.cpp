fa7aa5cf46365217ef97e23c21d5ab8a6063e3ef
Njord Lokebo <njordml@gmail.com>
2020-05-26 15:31:19 +0200

    cleaned up and manually accepted changes

--- src/chargeur.cpp
+++ src/chargeur.cpp
@@ -2,7 +2,6 @@
 #include "battery.h"
 
 
-<<<<<<< HEAD
 Chargeur::Chargeur(int t, Battery b):Device(),state(LOW) ,temps(t), batt(b)
 {
 
@@ -10,7 +9,7 @@ Chargeur::Chargeur(int t, Battery b):Device(),state(LOW) ,temps(t), batt(b)
 
 void Chargeur::chargeBatt()
 {
-  batt.setBattery(batt.checkBattery()+10);
+  batt.setBattery(b.checkBattery()+10);
 }
 
 void Chargeur::run()
@@ -27,28 +26,8 @@ void Chargeur::run()
       }
       else
       {
-        //chargeBatt();
-        cout << "-------------------------------------------------------charging" << endl;
+        chargeBatt();
       }
     }
-    sleep(1);
   }
 }
-=======
-Chargeur::Chargeur(int t):Device(),state(LOW),temps(t){}
-
-void Chargeur::run(){
-  //while loop qui regarde l'etat de Bouton?? 
-  //et CheckBatterie (si la batterie n'est pas à 100% - ON, à 100% OFF)
-  while(1){
-    /*if(ptrmem!=NULL)
-      state=*ptrmem;
-    if (state==LOW)
-     ;
-    else
-     ;
-    sleep(temps);
-    */
-   }
-}
->>>>>>> njord_branch2
